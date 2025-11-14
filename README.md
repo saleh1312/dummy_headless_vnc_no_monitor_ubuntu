241b5af180a7f493b75d3a281e22d93234312ab9
saleh1312 <profsoft1312@gmail.com>
2025-11-15 00:12:20 +0200

    sad

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ sudo systemctl start ssh
 
 
 sudo apt install git -y
+
+
+git clone https://github.com/saleh1312/dummy_headless_vnc_no_monitor_ubuntu.git
+
+
+chmod +x ./install.sh
+./install.sh
+
+chmod +x ./create_display.sh
+./create_display.sh :0
--- install.sh
+++ install.sh
@@ -3,12 +3,15 @@ set -euo pipefail
 
 
 echo "[install.sh] Running install/prepare.sh"
-bash "./install/prepare.sh"
+chmod +x ./install/prepare.sh
+bash ./install/prepare.sh
 
 echo "[install.sh] Running install/x11vnc_config.sh"
-bash "./install/x11vnc_config.sh"
+chmod +x ./install/x11vnc_config.sh
+bash ./install/x11vnc_config.sh
 
 echo "[install.sh] Running install/xorg_dummy_config.sh"
-bash "./install/xorg_dummy_config.sh"
+chmod +x ./install/xorg_dummy_config.sh
+bash ./install/xorg_dummy_config.sh
 
 echo "[install.sh] All install steps completed."
